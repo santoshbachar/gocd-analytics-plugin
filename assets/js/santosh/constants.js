992c89feeb775c7c0431829166f4ba147fbd7fb8
Santosh Kumar Bachar <santoshbachar@gmail.com>
2025-09-14 14:44:01 +0530

    Add StageRerunsViewState to manage Stage re-runs view

---
+++ assets/js/santosh/constants.js
@@ -0,0 +1,8 @@
+const StageRerunsViewState = Object.freeze({
+  CUSTOM: 'custom',
+  MONTH_TO_MONTH: 'month_to_month',
+  YEARLY: 'yearly',
+  YEAR_TO_YEAR: 'year_to_year'
+});
+
+export {StageRerunsViewState}
