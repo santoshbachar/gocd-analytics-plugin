d68e502612754dd5f0ed0db4f43d30355fb44eae
Santosh Kumar Bachar <santoshbachar@gmail.com>
2025-09-19 08:14:46 +0530

    Add view selector

--- assets/js/santosh/defination/stage-timeline/stage-reruns-header.js
+++ assets/js/santosh/defination/stage-timeline/stage-reruns-header.js
@@ -8,6 +8,7 @@ let dateFilterSelector = undefined;
 let pipelineSelector = undefined;
 let dateSelector = undefined;
 let requestOrderSelector = undefined;
+let viewSelector = undefined;
 
 async function stageRerunsHeader(pipelines, settingsDOM, dateSelectedEvent) {
 
@@ -16,6 +17,7 @@ async function stageRerunsHeader(pipelines, settingsDOM, dateSelectedEvent) {
     dateFilterSelector = document.getElementById("dateFilter");
     pipelineSelector = document.getElementById("pipeline");
     requestOrderSelector = document.getElementById("requestOrder");
+    viewSelector = document.getElementById("view");
 
     const dm = new DateManager();
     const datePicker = await dm.addDatelitePickerDiv(dateFilterSelector, dateSelectedEvent);
@@ -28,6 +30,8 @@ async function stageRerunsHeader(pipelines, settingsDOM, dateSelectedEvent) {
     }
 
     await addOptionsToSelect(pipelineSelector, ['*** All ***', ...pipelines]);
+
+    await addOptionsToSelect(viewSelector, ["Custom date", "Month to month", "Yearly", "Year to year"]);
 
     new SlimSelect({
         select: '#pipeline'
@@ -55,6 +59,14 @@ const custom_date_setting_html = `
       </div>
 `;
 
+const monthly_date_setting_html = `
+  <div style="display: flex; flex-direction: row; flex-grow: 1">
+         Month 1: <select id="month_1">
+          <option>January</option>
+         </select>
+      </div>
+`;
+
 async function addOptionHeader(settingsDOM) {
     settingsDOM.innerHTML = `
 <div style="position:relative; display: flex; flex-direction: column">
@@ -64,15 +76,12 @@ async function addOptionHeader(settingsDOM) {
     </div>
     <div style="display: flex; flex-direction: row; flex-grow: 5">
         for Pipeline: <select id="pipeline"></select>
+        view: <select id="view"></select>
     </div>
    </div>
    <div id="setting-1" style="display: flex; flex-direction: row">
       <div style="font-size:16px; flex-grow: 1"><b>Associated Settings</b></div>
-      <span id="dateFilter"></span>
-      
-      <div style="display: flex; flex-direction: row; flex-grow: 1">
-         Order: <select id="requestOrder"></select>
-      </div>
+      ${custom_date_setting_html}
    </div>
 </div>
 <hr>
